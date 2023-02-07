0225c02b9ee464fe0cac95c03908e2a7cf77caf3
Manuel Galindo <mfdogalindo@gmail.com>
2023-02-07 18:06:04 -0500

    Initial Commit

---
+++ MenuGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MenuGL
+//
+//  Created by Manuel Galindo on 7/02/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
